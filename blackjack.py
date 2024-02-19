8259f5838e5bce1b802ba5acd73e5203d4850aff
ChristianTStu <christiantstu@gmail.com>
2024-02-18 22:46:59 -0600

    Huge refactor of code

--- blackjack.py
+++ blackjack.py
@@ -1,135 +1,196 @@
+def main():
+    print('Welcome to the House of Blackjack. Where we always win!')
+
 import random
 
-dealer_deck = [
-    ['Ace', 'Spades'], 
-    ['2', 'Spades'], 
-    ['3', 'Spades'], 
-    ['4', 'Spades'], 
-    ['5', 'Spades'], 
-    ['6', 'Spades'], 
-    ['7', 'Spades'], 
-    ['8', 'Spades'], 
-    ['9', 'Spades'], 
-    ['10', 'Spades'], 
-    ['Jack', 'Spades'], 
-    ['Queen', 'Spades'], 
-    ['King', 'Spades'],
-    ['Ace', 'Hearts'],  
-    ['2', 'Hearts'], 
-    ['3', 'Hearts'], 
-    ['4', 'Hearts'], 
-    ['5', 'Hearts'], 
-    ['6', 'Hearts'], 
-    ['7', 'Hearts'], 
-    ['8', 'Hearts'], 
-    ['9', 'Hearts'], 
-    ['10', 'Hearts'], 
-    ['Jack', 'Hearts'], 
-    ['Queen', 'Hearts'], 
-    ['King', 'Hearts'],
-    ['Ace', 'Clubs'],
-    ['2', 'Clubs'], 
-    ['3', 'Clubs'],
-    ['4', 'Clubs'], 
-    ['5', 'Clubs'], 
-    ['6', 'Clubs'], 
-    ['7', 'Clubs'], 
-    ['8', 'Clubs'],
-    ['9', 'Clubs'], 
-    ['10', 'Clubs'], 
-    ['Jack', 'Clubs'],
-    ['Queen', 'Clubs'], 
-    ['King', 'Clubs'],
-    ['Ace', 'Diamonds'], 
-    ['2', 'Diamonds'], 
-    ['3', 'Diamonds'], 
-    ['4', 'Diamonds'], 
-    ['5', 'Diamonds'],
-    ['6', 'Diamonds'], 
-    ['7', 'Diamonds'], 
-    ['8', 'Diamonds'], 
-    ['9', 'Diamonds'], 
-    ['10', 'Diamonds'], 
-    ['Jack', 'Diamonds'], 
-    ['Queen', 'Diamonds'], 
-    ['King', 'Diamonds']
+suits = [
+    'Hearts',
+    'Diamonds',
+    'Spades',
+    'Clubs'
 ]
 
-used_cards = [
+cards_in_deck = [
+    'Ace',
+    '2',
+    '3', 
+    '4',
+    '5',
+    '6',  
+    '7', 
+    '8', 
+    '9', 
+    '10',
+    'Jack',
+    'Queen',
+    'King'
+]
+
+player_cards = [
 
 ]
 
-def burn_card():
-    rand_card = random.randint(0, 51)
-    burn_card = dealer_deck.pop(rand_card)
-    used_cards.append(burn_card)
+dealer_cards = [
+
+]
+
+aces = [
+    'Ace of Spades', 
+    'Ace of Hearts',
+    'Ace of Clubs',
+    'Ace of Diamonds'
+]
+
+twos= [
+    '2 of Spades', 
+    '2 of Hearts',
+    '2 of Clubs',
+    '2 of Diamonds'
+
+]
+
+threes = [
+    '3 of Spades', 
+    '3 of Hearts',
+    '3 of Clubs',
+    '3 of Diamonds'
+]
+
+fours = [
+    '4 of Spades', 
+    '4 of Hearts',
+    '4 of Clubs',
+    '4 of Diamonds'
+]
+
+fives = [
+    '5 of Spades', 
+    '5 of Hearts',
+    '5 of Clubs',
+    '5 of Diamonds'
+]
+
+sixes= [
+    '6 of Spades', 
+    '6 of Hearts',
+    '6 of Clubs',
+    '6 of Diamonds'
+]
+sevens = [
+    '7 of Spades', 
+    '7 of Hearts',
+    '7 of Clubs',
+    '7 of Diamonds'
+]
+eights = [
+    '8 of Spades', 
+    '8 of Hearts',
+    '8 of Clubs',
+    '8 of Diamonds'
+
+]
+nines = [
+    '9 of Spades', 
+    '9 of Hearts',
+    '9 of Clubs',
+    '9 of Diamonds'
+]
+tens = [
+    '10 of Spades', 
+    '10 of Hearts',
+    '10 of Clubs',
+    '10 of Diamonds',
+    'Jack of Spades',
+    'Jack of Hearts',
+    'Jack of Clubs',
+    'Jack of Diamonds',
+    'Queen of Spades',
+    'Queen of Hearts',
+    'Queen of Clubs',
+    'Queen of Diamonds', 
+    'King of Spades',
+    'King of Hearts',
+    'King of Clubs',
+    'King of Diamonds'
+
+]
+
 
 def deal_card():
-    rand_card = random.randint(0, len(dealer_deck))
-    connect = " of "
-    str_deck = dealer_deck[rand_card]
-    remove_card = dealer_deck.pop(rand_card)
-    used_cards.append(remove_card)
-    print('Your first card is...')
-    print(('The ') + (connect.join(str_deck)))
-
-def deal_2nd_card():
-    rand_card = random.randint(0, len(dealer_deck))
-    connect = " of "
-    str_deck = dealer_deck[rand_card]
-    remove_card = dealer_deck.pop(rand_card)
-    used_cards.append(remove_card)
-    print('Your second card is...')
-    print(('The ') + (connect.join(str_deck)))
-
+    rand_suit = random.randint(0, (len(suits) - 1))
+    rand_num = random.randint(0, (len(cards_in_deck) - 1))
+    rand_card = cards_in_deck[rand_num] + ' of ' + suits[rand_suit]
+    if rand_card in player_cards:
+        deal_card()
+    elif rand_card in dealer_cards:
+        deal_card()
+    else:
+        player_cards.append(rand_card)
+        return rand_card
+    
 def deal_card_dealer():
-    rand_card = random.randint(0, len(dealer_deck))
-    remove_card = dealer_deck.pop(rand_card)
-    used_cards.append(remove_card)
-    print("The dealer's first card is hidden.")
+    rand_suit = random.randint(0, (len(suits) - 1))
+    rand_num = random.randint(0, (len(cards_in_deck) - 1))
+    rand_card = cards_in_deck[rand_num] + ' of ' + suits[rand_suit]
+    if rand_card in player_cards:
+        deal_card_dealer()
+    elif rand_card in dealer_cards:
+        deal_card_dealer()
+    else:
+        dealer_cards.append(rand_card)
+        return rand_card
+    
+def player_hand_check():
+    hard_count = 0
+    ace_check = 0
+    for card in player_cards:
+        if card in aces:
+            hard_count += 1
+            ace_check += 11
+        elif card in twos:
+            hard_count += 2
+        elif card in threes:
+            hard_count += 3
+        elif card in fours:
+            hard_count += 4
+        elif card in fives:
+            hard_count += 5
+        elif card in sixes:
+            hard_count += 6
+        elif card in sevens:
+            hard_count += 7
+        elif card in eights:
+            hard_count += 8
+        elif card in nines:
+            hard_count += 9
+        elif card in tens:
+            hard_count += 10
 
-def deal_2nd_card_dealer():
-    rand_card = random.randint(0, len(dealer_deck))
-    connect = " of "
-    str_deck = dealer_deck[rand_card]
-    remove_card = dealer_deck.pop(rand_card)
-    used_cards.append(remove_card)
-    print("The dealer's second card is...")
-    print(('The ') + (connect.join(str_deck)))
+    soft_count = (hard_count - 1) + ace_check
 
+    if soft_count == 21:
+        print(f'Winner Winner, Chicken Dinner. Thats 21. Blackjack!!!')
+    elif soft_count > 21:
+        print(f'You have two Aces!! Wow :). That makes your hard count 22, becasue fo this you must choose your soft count of: {soft_count}. How would you like to proceed?')
+    elif soft_count > 0 and soft_count < 21:
+        print(f' You have an Ace! Your hard count is: {hard_count}. Your soft count of: {soft_count}. How would you like to proceed?')
+    else:
+        print(f'Your hard total is: {hard_count}.')
+        
 
-def round_deal_phase():
-    deal_card()
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
+    
+# Main Game Loop
+while True:
+    print('Your first card is...')
+    print(deal_card())
     deal_card_dealer()
-    deal_2nd_card()
-    deal_2nd_card_dealer()
-
-    
-# Introduction
-
-print('Welcome to the House of Blackjack. Where we always win!')
-play_response = input('Would you like to play? Yes or No:')
-
-if play_response.lower =='':
-    print('No input provided, please restart the program.')
-
-    exit()
-
-elif play_response.lower() =='yes':
-    pass
-
-elif play_response.lower() =='no':
-    print('Well okay! Please restart the program if you would like to use me later!')
-
-    exit()
-
-else:
-    print('Not a vaild response, please restart the program.')
-
-    exit()
-
-# Game Loop
-    
-while play_response.lower() =='yes':
-    round_deal_phase()
+    print("The dealer's first card is hidden.")
+    print('Your second card is...')
+    print(deal_card())
+    print("The dealer's second card is...")
+    print(deal_card_dealer())
+    player_hand_check()
     break
